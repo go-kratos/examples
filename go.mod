10de3f14d1233d7ce7c209c07f6461e53f585c8e
songzhibin97 <718428482@qq.com>
2024-09-03 14:35:37 +0800

    fix: upgrade kratos metrics example

--- go.mod
+++ go.mod
@@ -9,7 +9,6 @@ require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-kratos/gin v0.1.0
 	github.com/go-kratos/kratos/contrib/config/apollo/v2 v2.0.0-20220309025117-4387085047b9
-	github.com/go-kratos/kratos/contrib/metrics/prometheus/v2 v2.0.0-20220309025117-4387085047b9
 	github.com/go-kratos/kratos/contrib/opensergo/v2 v2.0.0-20220517145828-c8c870b77f70
 	github.com/go-kratos/kratos/contrib/registry/consul/v2 v2.0.0-20220309025117-4387085047b9
 	github.com/go-kratos/kratos/contrib/registry/discovery/v2 v2.0.0-20220309025117-4387085047b9
@@ -17,7 +16,7 @@ require (
 	github.com/go-kratos/kratos/contrib/registry/eureka/v2 v2.0.0-20220330020930-99a0646acb98
 	github.com/go-kratos/kratos/contrib/registry/nacos/v2 v2.0.0-20220309025117-4387085047b9
 	github.com/go-kratos/kratos/contrib/registry/zookeeper/v2 v2.0.0-20220309025117-4387085047b9
-	github.com/go-kratos/kratos/v2 v2.7.1
+	github.com/go-kratos/kratos/v2 v2.8.0
 	github.com/go-kratos/swagger-api v1.0.0
 	github.com/go-redis/redis/extra/redisotel v0.3.0
 	github.com/go-redis/redis/v8 v8.11.2
@@ -25,7 +24,7 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/google/wire v0.5.0
 	github.com/gorilla/handlers v1.5.1
-	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/mux v1.8.1
 	github.com/gorilla/securecookie v1.1.1
 	github.com/gorilla/websocket v1.4.2
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.0
@@ -33,24 +32,24 @@ require (
 	github.com/labstack/echo/v4 v4.6.3
 	github.com/nacos-group/nacos-sdk-go v1.0.9
 	github.com/nicksnyder/go-i18n/v2 v2.1.2
-	github.com/prometheus/client_golang v1.12.0
 	github.com/segmentio/kafka-go v0.4.27
 	github.com/sirupsen/logrus v1.8.1
 	github.com/soheilhy/cmux v0.1.4
 	github.com/xuri/excelize/v2 v2.6.1
 	go.etcd.io/etcd/client/v3 v3.5.1
 	go.mongodb.org/mongo-driver v1.10.3
-	go.opentelemetry.io/otel v1.23.1
+	go.opentelemetry.io/otel v1.24.0
 	go.opentelemetry.io/otel/exporters/jaeger v1.3.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.23.1
-	go.opentelemetry.io/otel/sdk v1.23.1
-	go.opentelemetry.io/otel/trace v1.23.1
+	go.opentelemetry.io/otel/metric v1.24.0
+	go.opentelemetry.io/otel/sdk v1.24.0
+	go.opentelemetry.io/otel/trace v1.24.0
 	go.uber.org/zap v1.19.0
 	golang.org/x/text v0.14.0
 	google.golang.org/genproto/googleapis/api v0.0.0-20240102182953-50ed04b92917
-	google.golang.org/grpc v1.61.0
+	google.golang.org/grpc v1.61.1
 	google.golang.org/grpc/examples v0.0.0-20220105183818-2fb1ac854b20 // indirect
-	google.golang.org/protobuf v1.32.0
+	google.golang.org/protobuf v1.33.0
 	gorm.io/driver/mysql v1.3.2
 	gorm.io/gorm v1.23.2
 )
--- metrics/main.go
+++ metrics/main.go
@@ -5,15 +5,14 @@ import (
 	"fmt"
 	"log"
 
-	prom "github.com/go-kratos/kratos/contrib/metrics/prometheus/v2"
-	"github.com/go-kratos/kratos/v2/middleware/metrics"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/metric"
 
 	"github.com/go-kratos/examples/helloworld/helloworld"
 	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/middleware/metrics"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/go-kratos/kratos/v2/transport/http"
-	"github.com/prometheus/client_golang/prometheus"
 )
 
 // go build -ldflags "-X main.Version=x.y.z"
@@ -23,20 +22,10 @@ var (
 	// Version is the version of the compiled software.
 	// Version = "v1.0.0"
 
-	_metricSeconds = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Namespace: "server",
-		Subsystem: "requests",
-		Name:      "duration_sec",
-		Help:      "server requests duration(sec).",
-		Buckets:   []float64{0.005, 0.01, 0.025, 0.05, 0.1, 0.250, 0.5, 1},
-	}, []string{"kind", "operation"})
+	meter = otel.Meter(Name)
 
-	_metricRequests = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "client",
-		Subsystem: "requests",
-		Name:      "code_total",
-		Help:      "The total number of processed requests",
-	}, []string{"kind", "operation", "code", "reason"})
+	_metricRequests metric.Int64Counter
+	_metricSeconds  metric.Float64Histogram
 )
 
 // server is used to implement helloworld.GreeterServer.
@@ -50,7 +39,16 @@ func (s *server) SayHello(ctx context.Context, in *helloworld.HelloRequest) (*he
 }
 
 func init() {
-	prometheus.MustRegister(_metricSeconds, _metricRequests)
+	var err error
+	_metricRequests, err = metrics.DefaultRequestsCounter(meter, metrics.DefaultServerRequestsCounterName)
+	if err != nil {
+		panic(err)
+	}
+
+	_metricSeconds, err = metrics.DefaultSecondsHistogram(meter, metrics.DefaultServerSecondsHistogramName)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
@@ -58,8 +56,8 @@ func main() {
 		grpc.Address(":9000"),
 		grpc.Middleware(
 			metrics.Server(
-				metrics.WithSeconds(prom.NewHistogram(_metricSeconds)),
-				metrics.WithRequests(prom.NewCounter(_metricRequests)),
+				metrics.WithSeconds(_metricSeconds),
+				metrics.WithRequests(_metricRequests),
 			),
 		),
 	)
@@ -67,12 +65,11 @@ func main() {
 		http.Address(":8000"),
 		http.Middleware(
 			metrics.Server(
-				metrics.WithSeconds(prom.NewHistogram(_metricSeconds)),
-				metrics.WithRequests(prom.NewCounter(_metricRequests)),
+				metrics.WithSeconds(_metricSeconds),
+				metrics.WithRequests(_metricRequests),
 			),
 		),
 	)
-	httpSrv.Handle("/metrics", promhttp.Handler())
 
 	s := &server{}
 	helloworld.RegisterGreeterServer(grpcSrv, s)
